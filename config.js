153a7f2c93432400fa7772273c81813538ab4468
ZDZ292 <nehemiahporter992@gmail.com>
2026-05-02 12:35:30 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-const CONFIG = {
+onst CONFIG = {
     // Replace with your unblocked OpenWeather key
     OPENWEATHER_API_KEY: '520798d83e7d393926e8c36dd888f952', 
     DEFAULT_LAT: 41.9742, // Chicago O'Hare
