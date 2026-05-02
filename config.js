9f096403316ec7f6c0f89f0178e6d6d03f3031d8
ZDZ292 <nehemiahporter992@gmail.com>
2026-05-02 12:25:52 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,7 @@
 const CONFIG = {
-    OPENWEATHER_API_KEY: '520798d83e7d393926e8c36dd888f952',
-    DEFAULT_PASSCODE: '1234'
+    OPENWEATHER_API_KEY: '520798d83e7d393926e8c36dd888f952', // The one you unblocked earlier
+    DEFAULT_LAT: 42.0451, // Evanston
+    DEFAULT_LON: -87.6877,
+    DEFAULT_PASSCODE: '1234',
+    UNITS: 'imperial'
 };
